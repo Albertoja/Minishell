7a9025b7c946608ba173eb46493b94bd85b3ab33
Alberto <99497451+Albertoja@users.noreply.github.com>
2023-03-06 15:04:23 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,16 +6,16 @@
 /*   By: aespinos <aespinos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:21:50 by aespinos          #+#    #+#             */
-/*   Updated: 2023/02/28 18:52:33 by aespinos         ###   ########.fr       */
+/*   Updated: 2023/03/06 14:45:16 by aespinos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "minishell.h"
+#include "minishell.h"
 
-// void	leaks(void)
-// {
-// 	system("leaks minishell");
-// }
+void	leaks(void)
+{
+	system("leaks minishell");
+}
 
 int	g_interactive = 0;
 
@@ -49,14 +49,17 @@ char	**copy_matrix(char **matrix)
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	**env;
+	char	*home;
 
-	//atexit(leaks);
+	home = NULL;
+	atexit(leaks);
 	if (argc != 1 || argv[1] || !envp)
 		return (0);
 	env = copy_matrix(envp);
+	home = ft_search_home(env, home);
 	ft_read_history();
 	no_ctrlprint();
 	signals_handlers();
-	ft_wait_for_input(env);
+	ft_wait_for_input(env, home);
 	ft_free_matrix(env);
 }
