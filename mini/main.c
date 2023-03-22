68a8cf16a7b408a1a78222d404b511947f896c6a
mariogalis <44349256+mariogalis@users.noreply.github.com>
2023-03-22 19:38:32 +0100

    alan

---
+++ mini/main.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magonzal <magonzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 18:21:50 by aespinos          #+#    #+#             */
+/*   Updated: 2023/03/22 18:35:35 by magonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// void	leaks(void)
+// {
+// 	system("leaks minishell");
+// }
+
+int	g_interactive = 0;
+
+int	count_str(char **matrix)
+{
+	int	ret;
+
+	ret = 0;
+	while (matrix[ret])
+		ret++;
+	return (ret);
+}
+
+char	**copy_matrix(char **matrix)
+{
+	char	**env;
+	char	**ret;
+
+	if (!matrix)
+		return (NULL);
+	env = malloc(sizeof(char *) * (count_str(matrix) + 1));
+	if (!env)
+		return (NULL);
+	ret = env;
+	while (*matrix)
+		*env++ = ft_strdup(*matrix++);
+	*env = NULL;
+	return (ret);
+}
+
+void	ft_shlvl(char **env)
+{
+	char	*shlvl;
+	char	*sub;
+	int		nbr;
+
+	if (!env || !*env)
+		return ;
+	while (ft_strncmp(*env, "SHLVL=", 6))
+		env++;
+	sub = ft_substr(*env, 6, UINT_MAX);
+	nbr = ft_atoi(sub) + 1;
+	free(sub);
+	sub = ft_itoa(nbr);
+	shlvl = ft_strjoinm("SHLVL=", sub);
+	free(sub);
+	free(*env);
+	*env = shlvl;
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	char	**env;
+
+	if (argc != 1 || argv[1] || !envp)
+		return (0);
+	env = copy_matrix(envp);
+	ft_shlvl(env);
+	ft_read_history();
+	no_ctrlprint();
+	signals_handlers();
+	ft_wait_for_input(env);
+	ft_free_matrix(env);
+}
---
+++ mini/pipex.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magonzal <magonzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/27 11:13:02 by magonzal          #+#    #+#             */
+/*   Updated: 2023/03/22 19:26:48 by magonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	select_redirection_pipex(t_all *aux, int *pip, int out)
+{
+	int	i;
+
+	i = -1;
+	while (aux->files && aux->files[++i])
+	{
+		if (aux->dir[i] == '2')
+			pipexoutpuredirection(aux, i);
+		else if (aux->dir[i] == '1')
+			pipexinputredirection(aux, i);
+		else if (aux->dir[i] == '4')
+			pipexappendredirection(aux, i);
+		else if (aux->dir[i] == '3')
+			heredocpip(aux, i, out);
+	}
+	if (aux->next != NULL && (!ft_strchr(aux->dir, '2')
+			&& !ft_strchr(aux->dir, '4')))
+		dup2(pip[1], STDOUT_FILENO);
+	else if (!ft_strchr(aux->dir, '2')
+		&& !ft_strchr(aux->dir, '4'))
+		dup2(out, STDOUT_FILENO);
+}
+
+void	childprocess(t_all *aux, char **envp, int *status, int *pip)
+{
+	int	out;
+
+	out = dup(STDOUT_FILENO);
+	select_redirection_pipex(aux, pip, out);
+	close(pip[0]);
+	close(pip[1]);
+	if (is_builtin(aux->cmds[0]) == 1)
+	{
+		ft_builtins(aux, envp, status);
+		exit(0);
+	}
+	if (execve(get_path(aux->cmds[0], envp), aux->cmds, envp) == -1)
+	{
+		ft_error("EPX : command not found", aux->cmds[0]);
+		exit(127);
+	}
+}
+
+void	pipex(t_all *head, char **envp, int *status)
+{
+	int		pip[2];
+	t_all	*aux;
+	pid_t	pid;
+
+	aux = head;
+	while (aux)
+	{
+		pipe(pip);
+		pid = fork();
+		if (pid == -1)
+			ft_error("ERROR: error on Fork", "\n");
+		if (pid == 0)
+			childprocess(aux, envp, status, pip);
+		if (aux->next != NULL)
+			dup2(pip[0], STDIN_FILENO);
+		close(pip[0]);
+		close(pip[1]);
+		aux = aux->next;
+	}
+	close(STDIN_FILENO);
+	waitpid(pid, status, 0);
+}
